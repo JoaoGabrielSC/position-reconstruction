9813f01a0630a8d939983081b8f2a66459b4d469
Joao <joaogabriel.sc99@gmail.com>
2025-03-10 08:53:06 -0300

    enhancing OOP

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from multiprocessing import Pool
 from src.calibration.parameters import Camera
 from src.calibration.matrices import MatrixProcessor
 from src.video_process.plot_results import plot_3d_points
-from src.video_process.process import compute_camera_matrices, process_video_for_camera, reconstruct_3d_points
+from src.video_process.process import VideoProcessor
 from src.parser import Parser
 
 videos_prefix_name: str = "camera-0"
 
 def main():
+    video_processor = VideoProcessor()
     parser = Parser().get_arguments()
     config_dir = parser.config_dir
     videos_path = parser.videos_path
@@ -19,10 +20,11 @@ def main():
 
     file_list = [f"{videos_path}/{videos_prefix_name}{i}.mp4" for i in range(4)]
 
+    print("Processando os 4 vídeos ao mesmo tempo com Paralelismo Multithread")
     with Pool(processes=4) as pool:
-        video_marker_positions = pool.map(process_video_for_camera, file_list)
+        video_marker_positions = pool.map(video_processor.process_video_for_camera, file_list)
 
-    camera_projection_matrices = compute_camera_matrices(cameras)
+    camera_projection_matrices = video_processor.compute_camera_matrices(cameras)
 
     matrices = []
     for i in range(len(video_marker_positions[0])):
@@ -31,7 +33,7 @@ def main():
         concatenated_matrix = processor.process_and_concatenate()
         matrices.append(concatenated_matrix)
 
-    resulting_A = reconstruct_3d_points(matrices)
+    resulting_A = video_processor.reconstruct_3d_points(matrices)
     plot_3d_points(resulting_A)
 
 if __name__ == "__main__":
